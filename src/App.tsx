faeda845297f02336991c8ddab3969c66c5524cb
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-02-01 00:44:02 -0300

    created edge and toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import * as Toolbar from '@radix-ui/react-toolbar'
 import ReactFlow, {
   addEdge,
   Background,
   Connection,
   ConnectionMode,
   Controls,
-  useEdgesState
+  useEdgesState,
+  useNodesState
 } from 'reactflow'
 import { zinc } from 'tailwindcss/colors'
 import 'reactflow/dist/style.css'
 
-import { Square } from './components/nodes/Squeare'
+import { Square } from './components/nodes/Square'
 import { useCallback } from 'react'
+import DefaultEdge from './components/edges/DefaultEdge'
 
 // Entidades: Nodes = tudo que vai aparecer em tela ex: quadrados, textos.
 //Edges = conexões, vai fazer a conexão entre um node e outro
 
 const NODE_TYPES = {
   square: Square
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge
 }
 
 const INITIAL_NODES = [
@@ -44,23 +51,50 @@ const INITIAL_NODES = [
 function App() {
   const [edges, setEdges, onEdgesChange] = useEdgesState([])
 
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES)
+
   const onConnect = useCallback((connection: Connection) => {
     return setEdges(edges => addEdge(connection, edges))
   }, [])
+
+  function addSquareNode() {
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+          x: 750,
+          y: 350
+        },
+        data: {}
+      }
+    ])
+  }
 
   return (
     <div className='w-screen h-screen'>
       <ReactFlow
         nodeTypes={NODE_TYPES}
-        nodes={INITIAL_NODES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
         edges={edges}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onNodesChange={onNodesChange}
         connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{ type: 'default' }}
       >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
+
+      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+        <Toolbar.Button
+          onClick={addSquareNode}
+          className='w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2'
+        />
+      </Toolbar.Root>
     </div>
   )
 }
